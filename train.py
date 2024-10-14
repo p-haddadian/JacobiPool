e090583d490c2d6ced6f3f0de8e9fae172f9e6a5
Parsa Haddadian <phadadian@gmail.com>
2024-10-14 09:43:24 +0330

    alpha/beta tunning

--- train.py
+++ train.py
@@ -135,11 +135,11 @@ def model_train(args, train_loader, val_loader):
 def objective(trial: optuna.Trial, args, train_loader, val_loader):
     # Hyperparametrs to tune
     args.num_hidden = trial.suggest_categorical('num_hidden', [32])
-    args.lr = trial.suggest_categorical('lr', [0.0005, 0.0001])
-    args.weight_decay = trial.suggest_categorical('weight_decay', [0.0001])
+    args.lr = trial.suggest_categorical('lr', [0.0005])
+    args.weight_decay = trial.suggest_categorical('weight_decay', [0.0005])
     args.pooling_ratio = trial.suggest_categorical('pooling_ratio', [0.35])
     args.dropout_ratio = trial.suggest_categorical('dropout_ratio', [0.2])
-    args.hop_num = trial.suggest_categorical('hop_num', [2, 3, 4])
+    args.hop_num = trial.suggest_categorical('hop_num', [4])
     args.a = trial.suggest_float('a', -1.0, 2.0, step=0.5)
     args.b = trial.suggest_float('b', -0.5, 2.0, step=0.5)
 
